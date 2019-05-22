90825c63f7c148869906b399c65a634c7a80ff84
Alvaro Picatoste <apicatoste@gmail.com>
2019-05-23 01:32:25 +0200

    New training dataset translated

--- Personal/Alvaro/alvaro_source.py
+++ Personal/Alvaro/alvaro_source.py
@@ -8,8 +8,16 @@ from langdetect import detect
 import os
 from textblob import TextBlob
 import time
+import langid
+from google.cloud import translate
+
+pd.set_option('max_columns', None)
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('precision', 4)
 
 ### Functions ###
+
 def stop_words():
     """Retrieve the stop words for vectorization -Feel free to modify this function
     """
@@ -28,26 +36,27 @@ def filter_hashtags(text):
     return re.sub("#\S+", "", text)
 
 
-def get_language(string):
-    # my_string = filter_hashtags(string)
-    # my_string = filter_mentions(my_string)
-    my_string = string
-    my_vector = my_string.split()
-    my_vector = ",".join(my_vector)
+def translate_string_google_cloud(string, source, target):
+    time.sleep(0.1)
+    translation = translate_client.translate(
+        string,
+        source_language=source,
+        target_language=target)
+    return translation['translatedText']
 
-    return detect(my_vector)
 
+def detect_language_google_cloud(string):
+    time.sleep(0.1)
+    return translate_client.detect_language(string)
+
+def get_language_id(google_translation):
+    return google_translation['language']
 
 def text_filtered(string):
     # my_string = filter_hashtags(string)
     # my_string = filter_mentions(my_string)
     my_string = string
     return my_string
-
-
-def get_lang(string):
-    tweet = TextBlob(string)
-    return tweet.detect_language()
 
 
 def save_submission(prediction):
@@ -61,102 +70,88 @@ def save_submission(prediction):
 if __name__ == '__main__':
     ROOT_PATH = "C:/workspace/my_repos/Capstone DS/Data/"
 
-    train_df = pd.read_excel(os.path.join(ROOT_PATH, "train-20190402-08_55_19.xlsx")) # Load the `train` file
-    test_df = pd.read_excel(os.path.join(ROOT_PATH, 'test-20190402-08_55_19-public.xlsx'))  # Load the `test` file
+    train_df = pd.read_excel(os.path.join(ROOT_PATH, "train-20190402-08_55_19.xlsx"))  # Load the `train` file
+    train_df = pd.read_excel(os.path.join(ROOT_PATH, 'test-20190402-08_55_19-public.xlsx'))  # Load the `test` file
 
     # remove 5715
-    train_df = train_df.drop([5715])
-    train_df.reset_index(inplace=True, drop=True)
+    #train_df = train_df.drop([5715])
+    #train_df.reset_index(inplace=True, drop=True)
 
     # detect lang
-    train_df['language'] = list(map(get_lang, train_df['text']))
-    train_df.head()
-    train_df['lang'] = list(map(get_language, train_df['text']))
+    translate_client = translate.Client()
+
+    ini = time.time()
+    train_df['no_of_characters'] = list(map(len, train_df['text']))
+    train_df['language'] = list(map(detect_language_google_cloud, train_df['text']))
+    train_df['language_id'] = list(map(get_language_id, train_df['language']))
+    end = time.time()
+    print("It took: ", end - ini, " seconds")
+
     train_df['text_modif'] = list(map(text_filtered, train_df['text']))  # to see text post filtering
     num_lang = train_df['language'].value_counts()
     plt.plot(num_lang)
 
     # Translation
     ini = time.time()
-    from gtrans import translate_text
-
     translations = []
     for idx, tweet in enumerate(train_df['text']):
-        if train_df.loc[idx, 'language'] == 'es':
-            translations.append(train_df.loc[idx, 'text_modif'])
+        print(f"{idx} / {len(train_df['text'])}")
+        source_language = train_df.loc[idx, 'language_id']
+        text_to_translate = train_df.loc[idx, 'text_modif']
+        if source_language == 'es':
+            translations.append(text_to_translate)
         else:
-            translation = translate_text(train_df.loc[idx, 'text_modif'], train_df.loc[idx, 'language'], "es")
+            translation = translate_string_google_cloud(text_to_translate,
+                                                        source_language,
+                                                        "es")
             translations.append(translation)
 
     train_df.loc[:, 'traducciones'] = translations
+    train_df.to_csv("C:/workspace/my_repos/Capstone DS/Data/test_traducido.csv", sep=';', index=False)
 
-    # por si se ha dejado alguno
-    a = True
-    while a == True:
-
-        empty = []
-        for idx, tweet in enumerate(train_df['traducciones']):
-            if tweet == "":
-                empty.append(idx)
-                print(idx)
-
-        if len(empty) == 0:
-            a = False
-
-        for idx, tweet in enumerate(train_df['traducciones']):
-            if tweet == "":
-                if train_df.loc[idx, 'language'] == 'es':
-                    train_df.loc[idx, 'traducciones'] = train_df.loc[idx, 'text_modif']
-                else:
-                    translation = translate_text(train_df.loc[idx, 'text_modif'], train_df.loc[idx, 'language'], "es")
-                    train_df.loc[idx, 'traducciones'] = translation
-
-    end = time.time()
-    print("It took: ", end - ini, " seconds")
-
-    train_df['day'] = [x.day for x in train_df.created_at]
-    train_df['month'] = [x.month for x in train_df.created_at]
-    train_df['year'] = [x.year for x in train_df.created_at]
-
-    train_df.to_csv(r"/Users/oliver/YOGA/Yoga/Personal/Oliver/train_df_tr2.csv", sep=";")
-
-    tfidf_vectorizer = TfidfVectorizer(
-        # tfidf_vectorizer = StemmedTfidfVectorizer(
-        sublinear_tf=True,
-        strip_accents='unicode',
-        min_df=3,
-        norm='l2',
-        token_pattern='(?u)\w\w+',  # r'[^0-9]\w{1,}',#r'#?[^0-9]\w\w+',
-        stop_words=stop_words(),
-        # ngram_range=(1,2),
-        # max_features=4000
-    )
-
-    X.columns
-    from sklearn.naive_bayes import BernoulliNB
-    from sklearn.model_selection import train_test_split
-
-    # Split train and test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
-    # My first Naive Bayes classifier!
-    clf = BernoulliNB()
-    clf.fit(X_train, y_train)
-    prediction = clf.predict(X_test)
-    print(np.mean([prediction == y_test]))
-
-    # 1 Train the classifier
-    clf = BernoulliNB()
-    clf.fit(X_train, y_train)
-
-    # 2 Predict the data (We need to tokenize the data using the same vectorizer object)
-    X_test = vectorizer.transform(test_df['text']).toarray()
-    prediction = clf.predict(X_test)
-
-    # 3 Create a the results file
-    output = pd.DataFrame({'Party': prediction})
-    output.index.name = 'Id'
-    output.to_csv('sample_submission.csv')
-
-
-    # TIP - Copy and paste this function to generate the output file in your code
+  #
+    # train_df['day'] = [x.day for x in train_df.created_at]
+    # train_df['month'] = [x.month for x in train_df.created_at]
+    # train_df['year'] = [x.year for x in train_df.created_at]
+    #
+    # train_df.to_csv(r"/Users/oliver/YOGA/Yoga/Personal/Oliver/train_df_tr2.csv", sep=";")
+    #
+    # tfidf_vectorizer = TfidfVectorizer(
+    #     # tfidf_vectorizer = StemmedTfidfVectorizer(
+    #     sublinear_tf=True,
+    #     strip_accents='unicode',
+    #     min_df=3,
+    #     norm='l2',
+    #     token_pattern='(?u)\w\w+',  # r'[^0-9]\w{1,}',#r'#?[^0-9]\w\w+',
+    #     stop_words=stop_words(),
+    #     # ngram_range=(1,2),
+    #     # max_features=4000
+    # )
+    #
+    # X.columns
+    # from sklearn.naive_bayes import BernoulliNB
+    # from sklearn.model_selection import train_test_split
+    #
+    # # Split train and test
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    #
+    # # My first Naive Bayes classifier!
+    # clf = BernoulliNB()
+    # clf.fit(X_train, y_train)
+    # prediction = clf.predict(X_test)
+    # print(np.mean([prediction == y_test]))
+    #
+    # # 1 Train the classifier
+    # clf = BernoulliNB()
+    # clf.fit(X_train, y_train)
+    #
+    # # 2 Predict the data (We need to tokenize the data using the same vectorizer object)
+    # X_test = vectorizer.transform(test_df['text']).toarray()
+    # prediction = clf.predict(X_test)
+    #
+    # # 3 Create a the results file
+    # output = pd.DataFrame({'Party': prediction})
+    # output.index.name = 'Id'
+    # output.to_csv('sample_submission.csv')
+    #
+    # # TIP - Copy and paste this function to generate the output file in your code
