08bd68ccaeaa4d586ab9d70e0a404bfa6d33e1d5
Alvaro Picatoste <apicatoste@gmail.com>
2019-06-13 08:19:26 +0200

    Fixed X train - X test for politicians classification

--- src/politicians_classification.py
+++ src/politicians_classification.py
@@ -1,22 +1,20 @@
 import sys
 import os
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-import utilities as funcs
 from sklearn.preprocessing import StandardScaler
+import functions as funcs
+from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-import pandas as pd
 import datetime as dt
-from keras.utils.np_utils import to_categorical
-
 
 
 def fill_nan_in_matrix(X, value):
     ix_list = np.argwhere(np.isnan(X)).tolist()
     for ix in ix_list:
         X[ix[0], ix[1]] = value
-
 
 def get_sentiment_features_df(ROOT_PATH, traintest_df, str, ):
     if (str == 'train'):
@@ -78,17 +76,20 @@ def get_language_df(df):
     return pd.DataFrame({'ca': np.where(df['language_id'] == 'ca', 1, 0),
                          'es': np.where(df['language_id'] == 'es', 1, 0),
                          'en': np.where(df['language_id'] == 'en', 1, 0),
-                         'other': df['language_id'].apply(
-                             lambda x: 1 if x not in {'ca', 'es', 'en'} else 0)
+                         'other': df['language_id'].apply(lambda x: 1 if x not in {'ca', 'es', 'en'} else 0)
                          })
 
 
+def get_amazon_sentiment_dummies_df(df):
+    return pd.DataFrame({'az_positive': np.where(df['amazon_sentiment'] == 'POSITIVE', 1, 0),
+                         'az_neutral': np.where(df['amazon_sentiment'] == 'NEUTRAL', 1, 0),
+                         'az_negative': np.where(df['amazon_sentiment'] == 'NEGATIVE', 1, 0),
+                         })
 
 def add_text_clean_col_to_df(df):
     df['text_clean'] = df['traducciones'].apply(lambda x: funcs.clean_text(x))
 
     return df
-
 
 def get_day_week(df):
     df.created_at = pd.to_datetime(df.created_at)
@@ -99,31 +100,65 @@ def get_day_week(df):
     day_week = ohe.fit_transform(day_week).toarray()
     return pd.DataFrame(day_week)
 
-
 def get_part_day(df):
     new_hours = df.created_at[df.created_at.dt.hour == 0]
     new_hours2 = new_hours + dt.timedelta(hours=1)
     df.created_at[df.created_at.dt.hour == 0] = new_hours2
-    part_day = pd.cut(df.created_at.dt.hour, [0, 6, 12, 18, 24], labels=['Night', 'Morning', 'Afternoon', 'Evening'])
+    part_day = pd.cut(df.created_at.dt.hour,[0,6,12,18,24],labels=['Night','Morning','Afternoon','Evening'])
     ohe = OneHotEncoder(handle_unknown='ignore')
     part_day = np.array(part_day)
     part_day = part_day.reshape(-1, 1)
     part_day = ohe.fit_transform(part_day).toarray()
     return pd.DataFrame(part_day)
+    
+def balance_dataset(df):
+
+    #target
+    y = df['username'].values
+    y1, y2 = np.unique(y, return_inverse=True)
+    values, tweet_counts = np.unique(y, return_counts=True)
+    politicians = pd.DataFrame({'values': values,
+                                'tweet_counts': tweet_counts})
+
+    max_tweet = np.max(politicians['tweet_counts'])
+
+    # todos los políticos con mismo número de tweets
+    pol_df_total = df.iloc[0:0]
+    for pol in politicians['values']:
+        pol_df = df[df.username == pol]
+        if pol == 'olallamarga':
+            pol_df = pol_df.loc[pol_df.index.repeat(20)]
+        pol_df = pd.concat([pol_df, pol_df[0:max_tweet-len(pol_df)]], axis=0)
+        pol_df_total = pd.concat([pol_df_total, pol_df], axis=0)
+    
+    pol_df_total.reset_index(inplace=True, drop=True)
+    df = pol_df_total.copy()
+    del(pol_df_total)
+    
+    #target
+    y = df['username'].values
+    y1, y2 = np.unique(y, return_inverse=True)
+    values, tweet_counts = np.unique(y, return_counts=True)
+    politicians = pd.DataFrame({'values': values,
+                                'tweet_counts': tweet_counts})
+    return df, politicians
+
 
 
 if __name__ == '__main__':
     # SET PATHS ##
-    ROOT_PATH = "C:/workspace/my_repos/Capstone DS/"
+    ROOT_PATH = "C:/workspace/Repositorios/Politicians/"
     PERSONAL_PATH = "Personal/Alvaro"
+
     os.chdir(os.path.join(ROOT_PATH, PERSONAL_PATH))
     sys.path.append(os.path.join(ROOT_PATH, PERSONAL_PATH))
     TRAIN_FPATH = os.path.join(ROOT_PATH, "Data/train_traducido.csv")
     TEST_FPATH = os.path.join(ROOT_PATH, "Data/test_traducido.csv")
 
-    ## Load data and Preprocess Data ##
     print("Data Loading...")
     train_df_tr = pd.read_csv(TRAIN_FPATH, delimiter=';')
+    train_df_tr.drop('party', axis=1, inplace=True)
+    train_df_tr, politicians = balance_dataset(train_df_tr)
     train_df_tr = remove_tweets_with_non_identified_language(train_df_tr)
     train_df_tr = add_text_clean_col_to_df(train_df_tr)
     train_df_counts = get_features_of_interest_counts(train_df_tr)
@@ -142,8 +177,10 @@ if __name__ == '__main__':
     test_df_sentiment = get_sentiment_features_df(ROOT_PATH, test_df_tr, str='test')
     test_features_count = count_features_and_scale(test_df_tr, test_df_counts)
 
+    y = train_df_tr['username'].values
+    y1, y2 = np.unique(y, return_inverse=True)
+
     ## MODEL ##
-    print("Launching model...")
     tfidf_vectorizer = funcs.StemmedTfidfVectorizer(
         sublinear_tf=True,
         max_df=0.25,
@@ -153,6 +190,8 @@ if __name__ == '__main__':
         ngram_range=(1, 1),
     )
 
+
+
     X_tfidf = tfidf_vectorizer.fit_transform(train_df_tr['text_clean']).toarray()
     X_tfidf = np.append(X_tfidf, train_df_languages, 1)
     X_tfidf = np.append(X_tfidf, train_features_count, 1)
@@ -160,34 +199,35 @@ if __name__ == '__main__':
     X_tfidf = np.append(X_tfidf, train_df_part_day, 1)
     X_tfidf = np.append(X_tfidf, train_df_sentiment, 1)
 
+    len(X_tfidf)
+    len(train_df_languages)
+
     X_tfidf_test = tfidf_vectorizer.transform(test_df_tr['text_clean']).toarray()
     X_tfidf_test = np.append(X_tfidf_test, test_df_languages, 1)
     X_tfidf_test = np.append(X_tfidf_test, test_features_count, 1)
+    X_tfidf_test = np.append(X_tfidf_test, test_df_sentiment, 1)
     X_tfidf_test = np.append(X_tfidf_test, test_df_day_week, 1)
     X_tfidf_test = np.append(X_tfidf_test, test_df_part_day, 1)
-    X_tfidf_test = np.append(X_tfidf_test, test_df_sentiment, 1)
     fill_nan_in_matrix(X_tfidf_test, value=0.5)
 
     le = LabelEncoder()
-    y_encode = le.fit_transform(train_df_tr['party'])
-    X_train, X_test, y_train, \
-    y_test, indices_train, indices_test = train_test_split(X_tfidf,
-                                                           y_encode,
-                                                           train_df_tr.index,
-                                                           test_size=0.25)
+    y_encode = le.fit_transform(y)
+    X_train, X_test, y_train, y_test,\
+    indices_train, indices_test = train_test_split(X_tfidf,
+                                                   y_encode,
+                                                   train_df_tr.index,
+                                                   test_size=0.25)
 
+    clf = SVC(C=1, kernel='linear')
+    assert len(X_train) == len(y_train)
+    clf.fit(X_train, y_train)
+    prediction = clf.predict(X_test)
+    print("Prediction: {}".format(np.mean([prediction == y_test])))
 
-    t0 = dt.datetime.now()
-    ## model selection ##
-    models, maxVote, Stacking = funcs.model_sel(X_train, y_train, X_test, y_test)
-
-    ## Choose the best model to train all the data and make the predictions ##
-    maxVote.fit(X_tfidf, y_encode)
-    predictions = maxVote.predict(X_tfidf_test)
-    predictions_ = np.argmax(to_categorical(predictions), axis=1)
-    predictions_ = le.inverse_transform(predictions_)
-
-    ## Create the results file ##
-    funcs.save_submission(predictions_, "yoga_submission")
-    t1 = dt.datetime.now()
-    print(t1-t0)
+    #
+    # #Create the results file
+    # assert len(X_tfidf) == len(y) == len(X_tfidf_test)
+    # clf.fit(X_tfidf, y)
+    # predictions = clf.predict(X_tfidf_test)
+    # len(X_tfidf_test)
+    # funcs.save_submission(predictions, "sample_submission_svc_politicians")
