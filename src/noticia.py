e540c748b43a9d5990bd2cb0b88f4c3ceef123ce
Alvaro Picatoste <alvaro.picatoste@ibm.com>
2019-06-29 11:03:14 +0200

    noticia.py template ready to be filled

--- src/noticia.py
+++ src/noticia.py
@@ -1,91 +1,30 @@
-import sys
-import os
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from parties_classification import remove_tweets_with_non_identified_language
-from parties_classification import add_text_clean_col_to_df
-from parties_classification import get_sentiment_features_df
-
-
-def giveme_max_min(feature):
-    x = np.max(train_df_tr[feature])
-    y = np.min(train_df_tr[feature])
-    return x, y
-
-
-def create_plot(feature1, feature2, feature_color):
-    x = feature1
-    y = feature2
-    fig = plt.figure()
-
-    categories = np.unique(train_df_tr[feature_color])
-    colors = np.linspace(0, 1, len(categories))
-    colordict = dict(zip(categories, colors))
-
-    train_df_tr["Color"] = train_df_tr[feature_color].apply(lambda x: colordict[x])
-    ax = fig.add_subplot(111)
-    ax.scatter(train_df_tr[x], train_df_tr[y], c=train_df_tr.Color)
-    ax.set_xlim(giveme_max_min(feature1)[1], giveme_max_min(feature1)[0])
-    ax.set_ylim(giveme_max_min(feature2)[1], giveme_max_min(feature2)[0])
-    ax.set(ylabel=feature2, xlabel=feature1)
-    ax.legend(loc='best')
-    return plt.show()
-
-
-def create_plot2(feature1, feature2, feature_color):
-    x = feature1
-    y = feature2
-
-    lista = list(np.unique(train_df_tr[feature_color]))
-    sns.set_style("whitegrid")
-    fg = sns.FacetGrid(data=train_df_tr,
-                       col=y,
-                       row=x,
-                       hue=feature_color, hue_order=lista, aspect=1.61)
-    fg.map(plt.scatter, 'Weight (kg)', 'Height (cm)').add_legend()
-    return plt.show()
-
-
-def create_plot3(feature1, feature2, feature_color):
-    x = feature1
-    y = feature2
-
-    list_of_series = [train_df_tr[x], train_df_tr[y], train_df_tr[feature_color]]
-    col_names = [x, y, feature_color]
-    df = pd.DataFrame(list_of_series, columns=col_names)
-    df = pd.concat(list_of_series, axis=1)
-
-    fg = sns.FacetGrid(data=df, hue=feature_color, hue_order=lista, aspect=1.61)
-    fg.map(plt.scatter, x, y, alpha=0.4).add_legend()
-    fig = fg.fig
-    fig.set_size_inches(10, 12)
-    return plt.show()
-
-
-def plot_uniqueval_column(feature):
-    y1, y2 = np.unique(train_df_tr[feature], return_inverse=True)
-    values, feature_ = np.unique(train_df_tr[feature], return_counts=True)
-    output = pd.DataFrame({'values': values,
-                           'feature': feature_})
-    ax = output['feature'].plot()
-    return ax
-
-
-def dateplot(x, y, **kwargs):
-    ax = plt.gca()
-    data = kwargs.pop("data")
-    data.plot(x=x, y=y, ax=ax, grid=False, **kwargs)
-
 
 if __name__ == '__main__':
+
+    ## Celda 1 Notebook ##
+
+    import os
+    import sys
+
     ROOT_PATH = "C:/workspace/my_repos/Capstone DS/"
     PERSONAL_PATH = "Personal/Alvaro"
-
+    sys.path.append(os.path.join(ROOT_PATH, 'src'))
     os.chdir(os.path.join(ROOT_PATH, PERSONAL_PATH))
     sys.path.append(os.path.join(ROOT_PATH, PERSONAL_PATH))
     TRAIN_FPATH = os.path.join(ROOT_PATH, "Data/train_traducido.csv")
+
+    ## Celda 2 Notebook ##
+
+    # All imports goes here
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns;
+    from parties_classification import remove_tweets_with_non_identified_language
+    from parties_classification import add_text_clean_col_to_df
+    from parties_classification import get_sentiment_features_df
+
+    ## Celda 3 Notebook ##
 
     ## Load data and Preprocess Data ##
     train_df_tr = pd.read_csv(TRAIN_FPATH, delimiter=';')
@@ -99,76 +38,64 @@ if __name__ == '__main__':
     tweets_df = train_df_tr.loc[:, cols_to_keep]
     tweets_df.created_at = pd.to_datetime(tweets_df.created_at)
     tweets_df = tweets_df.sort_values('created_at')
-    tweets_df.head()
+    print(tweets_df.head())
 
-    ##feature_color Make beautiful Plots ##
-    tweets_df.describe()
+    ## Celda 4 Notebook ##
 
+    x = 'created_at'
+    y = 'google_sentiment'
+    feature_color = 'party'
+    lista = list(np.unique(train_df_tr[feature_color]))
+    list_of_series = [train_df_tr[x], train_df_tr[y], train_df_tr[feature_color]]
+    col_names = [x, y, feature_color]
+    df = pd.DataFrame(list_of_series, columns=col_names)
+    df = pd.concat(list_of_series, axis=1)
+    df.created_at = pd.to_datetime(df.created_at)
+    df = df.groupby([pd.Grouper(key='created_at', freq='M'),
+                     pd.Grouper(key='party')]).agg(['count', 'mean'])
+    b = df.stack(level=0).reset_index()
+
+    ## Celda 5 Notebook ##
+
+    fg = sns.catplot(x="party", y="mean", kind="boxen",
+                     data=b)
+    fg.set_xticklabels(rotation=30)
+    fig = fg.fig
+    fig.set_size_inches(14, 10)
+
+    ## Celda 5b Notebook ##
+
+    ## AÑADIR MAPA DE CALOR CORRELACIONES SENTIMIENTOS
+    ## ENTRE POLITICOS
+
+    ## Celda 6 Noteboook ##
+
+    ## AÑADIR GRAFICA DE OLIVER CON LOS "COUNTS" ##
+
+    ## Celda 7 Noteboook ##
 
     f = {'google_sentiment': 'mean',
          'azure_sentiment': 'mean'
-         #'party': 'size'
          }
-
-  #  f_df = tweets_df.loc[tweets_df['party'] == x,]
     f_df = tweets_df[['created_at', 'party', 'azure_sentiment', 'google_sentiment']]
     f_df = f_df.set_index('created_at')
-    f_df = f_df.loc['2019-02-21':'2019-02-21']
-    f_df = f_df.groupby(['party']) \
+    f_df = f_df.loc['2018-10-01':'2018-12-30']
+    f_df = f_df.groupby([lambda x: x.year, lambda x: x.month, lambda x: x.day]) \
         .agg(f) \
-        .rename(columns={#'party': 'count',
-                         'google_sentiment': 'mean_daily_google_sentiment',
-                         'azure_sentiment': 'mean_daily_azure_sentiment'})
+        .rename(columns={  # 'party': 'count',
+        'google_sentiment': 'mean_daily_google_sentiment',
+        'azure_sentiment': 'mean_daily_azure_sentiment'})
     f_df.plot()
 
-    import pandas as pad
-    from numpy.random import randn
+    ## Celda 8 Notebook ##
+
+    ## Añadir Pequeña grafica con Distribucion de Sentimientos
+    ## de Amazon y comentar que su metodologia difiere de las
+    ## otras dos nubes ##
+
+  #Grafico de correlacion de sentimiento entre partidos. Semanal de los ultimos 6 meses.
+  #Corregir fechas de graficos y titulos, etc.
+  #Añadir etiqueta numerica al grafico de Boxplots y si se puede la media.
 
 
-    # lista = list(np.unique(tweets_df[feature_color]))
-    # list_of_series = [tweets_df[x], tweets_df[y], tweets_df[feature_color]]
-    # col_names = [x, y, feature_color]
-    # df = pd.DataFrame(list_of_series, columns=col_names)
-    # df = pd.concat(list_of_series, axis=1)
-    # fg = sns.FacetGrid(data=df, hue=feature_color, hue_order=lista, aspect=1.61)
-    # fg.map(plt.scatter, x, y).add_legend()
-    #
-    # df = tweets_df.groupby([pd.Grouper(key='created_at', freq='M'),
-    #                         pd.Grouper(key='party')]).agg(['count', 'mean'])
-    #
-    # a = pd.DataFrame(df.to_records(),
-    #                  columns=df.index.names + list(df.columns))
-    #
-    # b = df.stack().reset_index()
-    # b = df.stack(level=0).reset_index()
-    #
-    # sns.set()
-    # fg = sns.pairplot(b, hue='party').add_legend()
-    # fig = fg.fig
-    # fig.set_size_inches(11, 11)
-    #
-    # a.boxplot(by='created_at')
-    # a.set_xticklabels(rotation=30)
-    #
-    # fg = sns.factorplot(x='created_at', y='google_sentiment', hue='party',
-    #                     col='Sex',
-    #                     data=a, kind='bar')
-    #
-    # fg.set_xlabels('')
-    # fg.set_xticklabels(rotation=30)
-    #
-    # g = sns.FacetGrid(df)
-    # g = g.map_dataframe(dateplot, x, y)
-    #
-    #
-c_df = tweets_df.groupby("party").count()\
-    .rename(columns={'Id':'counts'})\
-    .reset_index(drop=False).iloc[:,[0,1]]
-c_dict = dict(zip(c_df.party,c_df.counts))
-c_df = tweets_df.groupby("party")\
-    .agg('mean')\
-    .sort_values('google_sentiment', ascending=False)
-c_df = c_df.loc[:,['google_sentiment']]
-print(c_df)
 
-c_df.plot.bar()
